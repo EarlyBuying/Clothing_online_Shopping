14f4dd6948c54dbbe7c1b3baf81218752a82994a
Upworld123 <upworld.shopping@gmail.com>
2023-01-03 16:35:40 +0530

    Log in page created

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,30 +1,26 @@
 import React from "react";
 import "./Login.css";
+import rightimg from "../img/right2.jpg";
+import rightimg2 from "../img/right1.jpg";
 
 const Login = () => {
   return (
-    <div className="mainContaine">
-      <div className="leftSide">
-        <div className="imagebox">{/* image here */}</div>
+    <div className="container">
+      <div className="right_Side">
+        <img src={rightimg} alt="" />
       </div>
-      <div className="rightSide">
-        <div>
-          <div className="mainForm">
-            <div className="content_email">
-              <h3>Email</h3>
-              <input type="text" placeholder="Input Your Email Here" />
-            </div>
-            <div className="content_password">
-              <h3>Password</h3>
-              <input type="text" placeholder="Input Your Password Here" />
-            </div>
-            <div className="checkBox">
-              <h4>CheckBox</h4>
-              <input type="checkbox" name="" id="" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <form action="" className="form">
+        <h2>Sign In</h2>
+        <input type="email" className="box" placeholder="Enter Email" />
+
+        <input type="password" className="box" placeholder="Enter Password" />
+        <input
+          type="submit"
+          className="box subBtn"
+          placeholder="Enter Password"
+        />
+        <a href="">Forget Password?</a>
+      </form>
     </div>
   );
 };
